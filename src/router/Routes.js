a2cbc931a38f445c577f75ceae955f2d0cef8787
umangcodes <umanga.eng@gmail.com>
2022-05-31 13:03:14 -0400

    feat: router updated with new layout

--- src/router/Routes.js
+++ src/router/Routes.js
@@ -3,14 +3,18 @@ import { useRoutes } from "react-router";
 import Home from "../pages/Home"
 import About from "../pages/About"
 import Error404 from "../pages/Error404"
-
+import BasicLayout from "../layout/BasicLayout";
 export default function Router(){
 
     let element = useRoutes([
         {
+            path: "/",
+            element: <BasicLayout />,
+            children: [
+                {
             path:"/",
             element: <Home />
-        },
+            },
         {
             path:"/about",
             element: <About />
@@ -19,6 +23,9 @@ export default function Router(){
             path:"*",
             element: <Error404 />
         },
+            ]
+        
+    }
     ])
     return element;
 }
