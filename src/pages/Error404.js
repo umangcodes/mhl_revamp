9499f77de60ffc2c35267e853abbea2c4538bb92
umangcodes <umanga.eng@gmail.com>
2022-05-31 12:52:22 -0400

    chore: basic page setup

---
+++ src/pages/Error404.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Error404() {
+  return (
+    <div>This page doesnt exist. Please Contact support at <a href="#">Support</a> </div>
+  )
+}
+
+export default Error404
