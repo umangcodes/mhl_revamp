049fdc061a85e0c5f2b22030f56e0acdcf7a9ba8
umangcodes <umanga.eng@gmail.com>
2022-06-01 13:04:07 -0400

    chore: custom colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:"#eeeeee",
+        primaryLight:"#ffffff",
+        primaryDark:"#bcbcbc",
+        secondary:"#90a4ae",
+        secondaryLight:"#c1d5e0",
+        secondaryDark:"#62757f",
+
+      }
+    },
     fontFamily:{
       'sans': ['sans-serif']
     }
