8eb8b7d22e71d658dc28a014f72577aeab75fc77
umangcodes <umanga.eng@gmail.com>
2022-06-01 12:08:14 -0400

    feat: margin added to the overall project.

--- src/layout/BasicLayout.js
+++ src/layout/BasicLayout.js
@@ -6,11 +6,11 @@ import Footer from "../components/navigation/Footer"
 
 function BasicLayout() {
   return (
-    <>
+    <div className="mx-5">
     <Header />
     <Outlet />
     <Footer />
-    </>
+    </div>
   )
 }
 
