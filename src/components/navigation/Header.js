e60a6fc4e013de62a40d6b3fa5859df5cbcd17a4
umangcodes <umanga.eng@gmail.com>
2022-06-01 13:08:56 -0400

    feat: sticky top location prop added and bg color changed

--- src/components/navigation/Header.js
+++ src/components/navigation/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Header() {
   return (
-    <div className='sticky flex justify-around align-baseline py-5'>
+    <div className='sticky top-0 flex justify-around align-baseline py-5 bg-primaryDark'>
       <div className='text-2xl'>MHL</div>
       <div className="flex w-48 justify-around">
         <div>Home</div>
