eb32d3e293f6ba1cba771ab4162c5344f0696b8e
umangcodes <umanga.eng@gmail.com>
2022-06-01 12:07:46 -0400

    feat: welcome panel created for home page. template created

---
+++ src/components/panels/homepage/Welcome.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Welcome() {
+  return (
+    <div className='h-80 flex flex-col justify-center border'>
+            <span className="text-center">Welcome tab/ <br/>Image place holder</span>
+    </div>
+  )
+}
+
+export default Welcome
