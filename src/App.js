dfb17e75d4cdb830475b5e69db6693cf3968f949
umangcodes <umanga.eng@gmail.com>
2022-06-01 13:08:14 -0400

    feat: background color changed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {BrowserRouter} from "react-router-dom"
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
+    <div className="App bg-primary">
       <Router />
     </div>
     </BrowserRouter>
