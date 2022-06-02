fd024958a73edf31a3f63e1ff360c4f94faf7e0e
umangcodes <umanga.eng@gmail.com>
2022-06-02 13:36:49 -0400

    feat: certificate added to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Welcome from "../components/panels/homepage/Welcome"
+import certificate from "../images/certificate.jpg"
+import Partners from "../components/panels/Partners"
 function Home() {
   return (
     <div className='min-h-screen'>
-      <span>Home page</span>
       <Welcome />
+      <img className="my-5" src={certificate}/>
 
     </div>
   )
