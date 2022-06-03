d94fdf7eb67409082cfe745824c81f5ebce67537
umangcodes <umanga.eng@gmail.com>
2022-06-03 13:41:34 -0400

    feat: error 404 page css adjusted

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Error404() {
   return (
-    <div>This page doesnt exist. Please Contact support at <a href="#">Support</a> </div>
+    <div className='h-screen flex flex-col justify-center align-center'>
+      <div className='text-center '>
+      This page does not exist. Please Contact support at <a href="#" className='font-semibold'>Support</a> 
+      </div>
+    </div>
   )
 }
 
