c361b87bca2a1128410154d4670f6ebebe2cbe83
umangcodes <umanga.eng@gmail.com>
2022-06-06 14:15:51 -0400

    feat: router links now active

--- src/components/navigation/Header.js
+++ src/components/navigation/Header.js
@@ -1,14 +1,21 @@
 import React from 'react'
-
+import logo from "../../images/MHL.png"
+import {Link} from "react-router-dom"
+import {openLinkInNewWindow} from "../../functions"
 function Header() {
   return (
-    <div className='sticky top-0 flex justify-around align-baseline py-5 bg-primaryDark'>
-      <div className='text-2xl'>MHL</div>
-      <div className="flex w-48 justify-around">
-        <div>Home</div>
-        <div>About</div>
+    <div className='sticky top-0 flex justify-around  py-5 bg-primaryDark'>
+      <div className='text-2xl align-center'>
+        <Link to="/">
+          <img className="obejct-scale-down w-36 lg:w-48" src={logo}/>
+        </Link>
       </div>
-
+      <div className="flex w-96 justify-around ">
+        <Link to="/" className="flex flex-col justify-center items-center hover:font-semibold w-48">Home</Link>
+        <Link to="/about" className="flex flex-col justify-center items-center  hover:font-semibold w-48">About</Link>
+        <Link to="/contact-us" className="flex flex-col justify-center items-center  hover:font-semibold w-48">Contact us</Link>
+        <button className='flex flex-col justify-center items-center  hover:font-semibold w-48' onClick={() => openLinkInNewWindow("https://results.mhlab.ca/patient/#/login")}>Results</button>
+      </div>
     </div>
   )
 }
