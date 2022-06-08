d033e189e54e84e59c002a498582cc2d225d69c3
umangcodes <umanga.eng@gmail.com>
2022-06-08 11:31:23 -0400

    feat: lab data added for mississauga location

---
+++ src/data/labs.js
@@ -0,0 +1,86 @@
+const labs = [{
+    area:'Mississauga',
+    labId: 470,
+    address1: "306 - 3024 Hurontario St ",
+    city: "Mississauga",
+    province: "ON",
+    postalCode: "L5B 4M4",
+    contact: {
+        phone: 9053061580,
+        fax: 9053067077
+    },
+    operationHours:{
+        mon:{
+            start: "0800",
+            end: "1600"
+        },
+        tue:{
+            start: "0800",
+            end: "1600"
+        },
+        wed:{
+            start: "0800",
+            end: "1600"
+        },
+        thu:{
+            start: "0800",
+            end: "1600"
+        },
+        fri:{
+            start: "0800",
+            end: "1600"
+        },
+        sat:{
+            start: "0800",
+            end: "1600"
+        },
+        sun:{
+            start: 0,
+            end: 0
+        }
+    }
+},
+{
+    area:'Mississauga',
+    labId: 474,
+    address1: "204 - 3420 Hurontario St ",
+    city: "Mississauga",
+    province: "ON",
+    postalCode: "L5B 4A9",
+    contact: {
+        phone: 9058488100,
+        fax: 9058488101
+    },
+    operationHours:{
+        mon:{
+            start: "0830",
+            end: "1630"
+        },
+        tue:{
+            start: "0830",
+            end: "1630"
+        },
+        wed:{
+            start: "0830",
+            end: "1630"
+        },
+        thu:{
+            start: "0830",
+            end: "1630"
+        },
+        fri:{
+            start: "0830",
+            end: "1630"
+        },
+        sat:{
+            start: "0800",
+            end: "1300"
+        },
+        sun:{
+            start: 0,
+            end: 0
+        }
+    }
+}]
+
+export default labs
