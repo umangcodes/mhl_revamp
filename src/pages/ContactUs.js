eaab1f5ca4339f3973abedcee71b4eb0a9b58705
umangcodes <umanga.eng@gmail.com>
2022-06-08 11:31:47 -0400

    feat: contact form dump

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,6 +1,7 @@
 import React,{useEffect, useState} from 'react'
 import validator from "validator"
 import Datepicker from "flowbite-datepicker/Datepicker"
+import 'flowbite';
 function ContactUs() {
     // const [phoneNumber, setPhoneNumber] = useState()
     // const [formError, setFormError] = useState();
@@ -27,7 +28,7 @@ function ContactUs() {
               <div className="shadow overflow-hidden sm:rounded-md">
                 <div className="px-4 py-5 bg-white sm:p-6">
                   <div className="grid grid-cols-6 gap-6">
-                    <div className="col-span-6 sm:col-span-3">
+                    <div className="col-span-6 sm:col-span-4">
                       <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
                         First name
                       </label>
@@ -40,7 +41,7 @@ function ContactUs() {
                       />
                     </div>
 
-                    <div className="col-span-6 sm:col-span-3">
+                    <div className="col-span-6 sm:col-span-4">
                       <label htmlFor="last-name" className="block text-sm font-medium text-gray-700">
                         Last name
                       </label>
@@ -66,7 +67,7 @@ function ContactUs() {
                       />
                     </div>
 
-                    <div className="col-span-6 sm:col-span-3">
+                    <div className="col-span-6 sm:col-span-4">
                       <label htmlFor="country" className="block text-sm font-medium text-gray-700">
                         Subject
                       </label>
@@ -78,14 +79,6 @@ function ContactUs() {
                         className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                       />
                     </div>
-
-                    <div class="relative">
-                        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                            <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
-                        </div>
-                        <input datepicker datepicker-autohide type="text" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date" />
-                    </div>
-                
                   </div>
                 </div>
                 <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
