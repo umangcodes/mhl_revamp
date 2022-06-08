4a2b870d69515b94f52914b50c893197ab1b26c2
umangcodes <umanga.eng@gmail.com>
2022-06-08 11:30:42 -0400

    chore: flowbite added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {
@@ -18,5 +19,7 @@ module.exports = {
       'sans': ['sans-serif']
     }
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
