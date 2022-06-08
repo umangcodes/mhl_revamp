64a2446f7ac03b7a4737486cc3c72492639ee62b
umangcodes <umanga.eng@gmail.com>
2022-06-08 11:33:33 -0400

    feat: minor changes in footer

--- src/components/navigation/Footer.js
+++ src/components/navigation/Footer.js
@@ -3,10 +3,11 @@ import LocationOnIcon from '@mui/icons-material/LocationOn';
 import PhoneIcon from '@mui/icons-material/Phone';
 import FaxIcon from '@mui/icons-material/Fax';
 import locations from "../../data/locations"
+import SummarizeIcon from '@mui/icons-material/Summarize';
 function Footer() {
   return (
     <div className='border-t my-5 py-5'>
-      <div className=' h-96 bg-primary flex flex-col justify-around lg:flex-row px-5'>
+      <div className=' h-96 bg-primary flex flex-col justify-around lg:justify-start lg:px-48 lg:flex-row px-5'>
         <div className='flex flex-col justify-around lg:flex-row'>
           <div className='lg:mx-5'>
           <span className='font-semibold text-lg'>Support</span>
@@ -23,14 +24,15 @@ function Footer() {
             <span className='font-semibold text-lg'>Main Facility</span>
             <div className='flex my-2'>
             <LocationOnIcon />
-            <a href={locations.mainLab}>1216 Lawrence Ave W, North York, ON</a>
+            <a href={locations.mainLab}>1216 Lawrence Ave W,<br/> North York, ON M6A 1E3 </a>
             </div>
           </div>
+          <div className='lg:mx-5 flex flex-col '>
+            <span className='font-semibold text-lg '>Services</span>
+            <button className='flex my-2' >
+              <SummarizeIcon /> <a href="https://results.mhlab.ca/patient/#/login/">Check Your Results</a></button>
+          </div>
         </div>
-        <div>
-        <button className='' > <a href="https://results.mhlab.ca/patient/#/login/">Check Results</a></button>
-        </div>
-        <div className='flex'>Map</div>
       </div>
       <div className='flex justify-center py-5 font-semibold text-center'>Copyright © 2022 Med-Health Laboratories Ltd. All rights reserved</div>
     </div>
