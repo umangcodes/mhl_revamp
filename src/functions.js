ff8554d6ba6e585481c13e345f2b8c86e1c8cd89
umangcodes <umanga.eng@gmail.com>
2022-06-08 15:26:48 -0400

    feat: time conversion function to display time in12 hour cycle created

--- src/functions.js
+++ src/functions.js
@@ -2,4 +2,17 @@ function openLinkInNewWindow(url){
     window.open(url);
 }
 
-export {openLinkInNewWindow}
+function convertTime(inputString){
+    const time = parseInt(inputString)
+    if(time < 1200){
+        return `${(time/100).toFixed(2)} am`
+    }
+    else if (time > 1200){
+        return `${((time-1200)/100).toFixed(2)} pm`
+    }
+    else{
+        return 'N/A'
+    }
+}
+
+export {openLinkInNewWindow, convertTime}
