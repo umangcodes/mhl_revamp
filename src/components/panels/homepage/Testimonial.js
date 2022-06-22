364e6d5b19229a2f7af69c020ad81ec96822032b
umangcodes <umanga.eng@gmail.com>
2022-06-22 15:24:07 -0400

    feat: testimonials added

---
+++ src/components/panels/homepage/Testimonial.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Testimonial() {
+  return (
+    <div>
+        <span>Proudly serving our communities with expertise in laboratory medicine.</span>
+            
+    </div>
+  )
+}
+
+export default Testimonial
